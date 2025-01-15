240f15320c3c69887a32a24ff21521cd46221833
BenFuchs <bencode@gmail.com>
2025-01-15 14:46:14 +0200

    minor fix

--- frontend/my-new-app/src/App.tsx
+++ frontend/my-new-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { Routes, Route, Link, useLocation } from 'react-router-dom';
 import { FaMoon, FaSun } from 'react-icons/fa'; 
 import Lobby from './features/Lobby/Lobby';
